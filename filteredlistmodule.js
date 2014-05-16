4e4852b91e479e7cb5f7f13dbe02fbf59731d92b
Teague Hopkins <teague@teaguehopkins.com>
2014-05-16 01:10:58 -0400

    Solved MakeItModular

--- filteredlistmodule.js
+++ filteredlistmodule.js
@@ -1,40 +1,29 @@
-module.exports = function (args) { /* ... */ }
-
+module.exports = function (path, ext, callback) {
 var fs = require('fs');
-var arrayArguments = process.argv;
-var path = arrayArguments[0];
-var ext = arrayArguments[1];
 var filteredList = [];
-
-console.log("In Module");
-console.log(arrayArguments);
 
 function filter(filename)
 {
-  //Takes a filename and if the extension matches global variable 'ext', it pushes the filename to array 'filteredList'
+  //this function takes a filename and if the extension matches global variable 'ext', it pushes the filename to array 'filteredList'
   var array = filename.toString().split(".");
   if (array[1]==ext){filteredList.push(filename);}
   return;
 }
 
-//Takes a file path as input and calls back with an array of filenames found at that path.
 fs.readdir(path, function (err, list) {
-  if (err) {
-    //handle error
-    console.log(err);
-    }
-
+  if (err) return callback(err);
   var filenames = list;
 
 
-//module print section for debugging before the callback
   filenames.forEach(function(x){filter(x)});
-  console.log("Module Print");
+
+/*
+  //Prints out the contents of the filteredList
   filteredList.forEach(function(x){
-    console.log(x);
-
-
-    callback(null, x)}
+  console.log("X: " + x)}
   )
+*/
 
-  });
+  callback(null, filteredList);
+});
+}
--- makeitmodular.js
+++ makeitmodular.js
@@ -5,9 +5,13 @@ var ext = arrayArguments[3];
 var filteredList = [];
 
 //console.log(ext);
-console.log("In Main");
-console.log(arrayArguments);
 
-mymodule(path, ext, function(err, data){
-  console.log(data);
+//console.log(arrayArguments);
+
+var filteredList = mymodule(path, ext, function(err, data){
+  //console.log("In Main");
+  //console.log("Data: " + data);
+  data.forEach(function(x){
+    console.log(x);}
+  )
 });
